25502195377d94406947bbde52b0d8d542443c6c
Franco Abaroa <franco9395@gmail.com>
2016-09-19 12:17:36 -0700

    should have an `eat` method that is inherited from grub and should have treasure chest property passing

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,10 +1,13 @@
 var RetiredForagerBee = function() {
   ForagerBee.call(this);
+  // Grub.call(this);
   this.age = 40;
   this.job = 'gamble';
   this.canFly = false;
   this.color = 'grey';
 };
+
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
 
 RetiredForagerBee.prototype.forage = function () {
   return 'I am too old, let me play cards instead';
