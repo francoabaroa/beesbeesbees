ab6e73f78633b022b480f36a2f456723a436f1c2
Franco Abaroa <franco9395@gmail.com>
2016-09-19 12:09:37 -0700

    should have a `treasureChest` property that is set to an empty array `[]` - test passed

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -4,6 +4,7 @@ var ForagerBee = function() {
   this.age = 10;
   this.job = 'find pollen';
   this.canFly = true;
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Grub.prototype);
